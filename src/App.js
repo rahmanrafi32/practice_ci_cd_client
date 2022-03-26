0edd121c34ea807c79d3a169fe9e352b97a44137
Minhazur Rahman <rafi.rahman03@yahoo.com>
2022-03-26 23:43:05 +0600

    dummy commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Welcome to GitHub Action</h1>
       <p>Am practicing ci cd with github actions</p>
+        <p>Getting problem with nginx</p>
     </div>
   );
 }
